49ccd04f05258adccbcdf2b250bfdc24b20d1b46
Sampiquepas <92890152+Sampiquepas@users.noreply.github.com>
2021-10-28 11:46:40 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+let distance = 0
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    if (distance > 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (distance < 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+})
